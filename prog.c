6c4dff1b7e928a9137f2e039aace3f50a2c9500f
Miguel <miguelneto.1995@hotmail.com>
2017-04-18 03:28:36 -0300

    Implementing desenvolvimentoFunc

--- prog.c
+++ prog.c
@@ -5,6 +5,11 @@ void masterFunc()
 	puts("This is the original function on the 1st commit.");
 }
 
+void desenvolvimentoFunc()
+{
+	puts("You called the desenvolvimentoFunc() function.");
+}
+
 int main()
 {
 	masterFunc();
